a84be67d0f3dc1012781c1f24aabf4a40b7f75d4
amaleshkk <amaleshkk@gmail.com>
2023-11-28 23:26:11 +0530

    data types complete

---
+++ Day2/remaining_week.py
@@ -0,0 +1,10 @@
+# Life remaining weeks
+
+age = int(input("Enter your age: "))
+
+remaining_years = 90 - age
+
+remaining_weeks = remaining_years * 52
+
+print(f"You have {remaining_weeks} remaining in your life")
+
---
+++ Day2/tip_calculator.py
@@ -0,0 +1,15 @@
+# simple tip calculator program
+
+print("Welcome to the tip calculator")
+
+total_bill = float(input("What was the total bill: "))
+tip = int(input("What percentage tip would you like to give, (10, 12 or 15): "))
+total_people = int(input("How many people to split the bill: "))
+
+bill_with_tip = total_bill * tip / 100 + total_bill
+
+bill_per_person = bill_with_tip / total_people
+final_amount = round(bill_per_person, 2)
+final_amount = "{:.2f}".format(bill_per_person)
+
+print(f"Each person should pay {final_amount}")
